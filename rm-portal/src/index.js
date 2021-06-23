1b34a72292b382dc2fa4e37f758f3768850abb91
nashsolon <i.solon@wustl.edu>
2021-06-23 15:09:40 -0400

    amplify

--- rm-portal/src/index.js
+++ rm-portal/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <BrowserRouter>
