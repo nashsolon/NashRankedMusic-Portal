123d40670d68c3b59c883311477519fec531ed1f
nashsolon <i.solon@wustl.edu>
2021-06-24 17:13:53 -0400

    puppeteer stuff

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,23 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use("/api/v1/campaigns", campaigns)
+app.use("/api/v1/campaigns", campaigns
+    // , function(req, res, next){
+    // res.header("Access-Control-Allow-Origin", "http://ec2-3-15-26-27.us-east-2.compute.amazonaws.com:3000"); // update to match the domain you will make the request from
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    // res.header({"Access-Control-Allow-Origin": "*"})
+    // next();
+// }
+)
 // Message if go to route that DNE
 app.use("*", (req, res) => res.status(404).json({error: "not found"}))
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://ec2-3-15-26-27.us-east-2.compute.amazonaws.com:3000"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
+  
+
 
 // Export app as module
 export default app 
--- rm-portal/src/http-common.js
+++ rm-portal/src/http-common.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/campaigns",
+    // baseURL: "http://localhost:5000/api/v1/campaigns",
+    baseURL: "http://ec2-3-15-26-27.us-east-2.compute.amazonaws.com:5000/api/v1/campaigns",
+    // baseURL: "http://169.254.169.254/latest/meta-data/",
+    // baseURL: "http://172.31.12.224/:5000/api/v1/campaigns",
     headers: {
         "Content-type": "application/json"
     }
